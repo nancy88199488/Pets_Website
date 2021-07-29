68f18e2eb17985bb16c5c2194b42cf6d46af2efc
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-29 04:14:29 +0300

    made changes to the files

--- PetParadise/admin.py
+++ PetParadise/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pets,UserProfile,OrderPets
+
 
 # Register your models here.
+admin.site.register(Pets)
+admin.site.register(UserProfile)
+admin.site.register(OrderPets)
--- PetParadise/views.py
+++ PetParadise/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+from PetParadise.forms import Registration
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
-def pet(request):
-    context = {}
-    return render(request,'pet/pet.html', context)
 
-def checkout(request):
-    context = {}
-    return render(request,'pet/checkout.html', context)
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
 
-def cart(request):
-    context = {}
-    return render(request,'pet/cart.html', context)        
+def register(request):
+  if request.method == 'POST':
+    form = Registration(request.POST)
+    if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(request,f'Account for {username} created,you can now login')
+      return redirect('login')
+  else:
+    form = Registration()
+  return render(request,'registration/register.html',{"form":form}) 
+
+
+
+
+
+
+
+
+
