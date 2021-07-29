52bcab811812a261ce76da5440bd2d6803f57fa2
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-29 06:06:36 +0300

    update all the templates file

--- PetParadise/urls.py
+++ PetParadise/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path,include
+from django.urls import path
 from . import views 
-# from .views import (ItemDetailView,add_to_cart,remove_from_cart)
+from .views import (PetDetailView,HomeView,add_to_cart,remove_from_cart)
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     path('',views.index, name='index'),
+    path('', HomeView.as_view(), name='home'),
     path('register/', views.register, name='register'),
-    path('account/', include('django.contrib.auth.urls')),
-    # path('pets/<slug>/', ItemDetailView.as_view(), name='pets'),
-    # path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    # path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    # path('account/', include('django.contrib.auth.urls')),
+    path('pets/<slug>/', PetDetailView.as_view(), name='pets'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
 
 
     
--- pets/urls.py
+++ pets/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
-from django.contrib.auth import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('PetParadise.urls')),
+    path('',include('PetParadise.urls',namespace='PetParadise')),
+    path('account/',include('allauth.urls'))
    
 ]
+
